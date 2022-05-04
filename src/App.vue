374ca01d0e842a3187d8dcb05cf93dbe95526352
gianmarco <gianmarco.culqui.ikaros@gmail.com>
2022-05-04 16:11:08 +0200

    up

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <app-header/>
+    
+  </div>
+</template>
+
+<script>
+
+import appHeader from './components/appHeader.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    appHeader,
+    
+    
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+
+</style>
---
+++ src/components/appHeader.vue
@@ -0,0 +1,83 @@
+<template>
+<header>
+    <nav>
+        <img src="../assets/images/dc-logo.png" alt="">
+        <ul>
+            <li v-for="(item, index) in links" :key="index">{{item.testo}}</li>
+        </ul>
+
+    </nav>
+    
+
+</header>
+</template>
+
+<script>
+
+
+export default {
+    name: 'appHeader',
+    data(){
+        return{
+            links:[
+                {
+                    testo:"characters",
+                    link:"",
+                    active:"false",
+                },
+                {
+                    testo:"comics",
+                    link:"",
+                    active:"false",
+                },
+                {
+                    testo:"movies",
+                    link:"",
+                    active:"false",
+                },
+                {
+                    testo:"tv",
+                    link:"",
+                    active:"false",
+                },
+                {
+                    testo:"games",
+                    link:"",
+                    active:"false",
+                },
+                {
+                    testo:"collectibles",
+                    link:"",
+                    active:"false",
+                },
+                {
+                    testo:"videos",
+                    link:"",
+                    active:"false",
+                },
+                {
+                    testo:"fans",
+                    link:"",
+                    active:"false",
+                },
+                {
+                    testo:"news",
+                    link:"",
+                    active:"false",
+                },
+                {
+                    testo:"shop",
+                    link:"",
+                    active:"false",
+                }
+            ]
+        }
+    }
+    
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
